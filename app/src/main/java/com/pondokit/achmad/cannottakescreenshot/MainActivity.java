ab33e8087621423698901d329d83ed767ed6f141
achmadqomarudin <achmadprogrammer@gmail.com>
2019-05-09 10:01:16 +0700

    Apps Cannot Take Screenshot

---
+++ app/src/main/java/com/pondokit/achmad/cannottakescreenshot/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pondokit.achmad.cannottakescreenshot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+    }
+}
